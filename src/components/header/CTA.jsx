69fbd67055559182fcf5b9f3313d0249454af953
Adetola <adetolafadairo@gmail.com>
2022-05-23 01:46:12 -0700

    adding button classNames

--- src/components/header/CTA.jsx
+++ src/components/header/CTA.jsx
@@ -4,8 +4,8 @@ import CV from '../../assets/cv.pdf'
 const CTA = () => {
   return (
     <div className='cta'>
-      <a href={CV} downlaod>Download CV</a>
-      <a href='#contact'>Let's Talk</a>
+      <a href={CV} downlaod className='btn'>Download CV</a>
+      <a href='#contact' className='btn btn-primary'>Let's Talk</a>
     </div>
   )
 }
