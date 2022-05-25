fb155e425a300a2936e97714ea8d5d37c30a835e
Adetola <adetolafadairo@gmail.com>
2022-05-25 12:57:54 -0700

    importing and using react-icons in about component

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,9 +1,57 @@
-import React from 'react'
-import 'components/about/about.css'
+import React from 'react';
+import './about.css';
+import ME from '../../assets/me-about.jpg';
+import { FaAward } from 'react-icons/fa';
+import { FiUsers } from 'react-icons/fi';
+import { VscFolderLibrary } from 'react-icons/vsc';
 
 const About = () => {
   return (
-    <div>About</div>
+    <section id='about'>
+      <h5>Get To Know</h5>
+      <h2>About Me</h2>
+
+      <div className='container about__container'>
+        <div className="about__me">
+          <div className="about__me-image">
+            <img src={ME} alt='About Image' />
+          </div>
+        </div>
+
+        <div className="about__content">
+          <div className="about__cards">
+            <article className='about__card'>
+              <FaAward className='about__icon' />
+              <h5>Ecperience</h5>
+              <small>1 Year</small>
+            </article>
+
+            <article className='about__card'>
+              <FiUsers className='about__icon' />
+              <h5>Clients</h5>
+              <small>200+ Worldwide</small>
+            </article>
+
+            <article className='about__card'>
+              <VscFolderLibrary className='about__icon' />
+              <h5>projects</h5>
+              <small>80+ Completed</small>
+            </article>
+          </div>
+
+          <p>
+
+            I am a developer with a specialization in Full Stack operations dedicated to making applications run smoothly.  
+            As a natural leader and great team player, I bring an understanding and know the importance of being dependable and reliable to the company and my coworkers.
+            I am passionate about the industry because of the opportunities for personal growth and development,
+             also to being able to make meaningful contributions to projects that I value technically.
+          </p>
+
+          <a href='#contact' className='btn btn-primary'>Let's Talk</a>
+        </div>
+      </div>
+
+    </section>
   )
 }
 
