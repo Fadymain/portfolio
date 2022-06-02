f31f8a0beacb09172a3dd195640a90e6a8c44fc8
Adetola <adetolafadairo@gmail.com>
2022-06-01 20:36:15 -0700

    putting skill and experince level in a div

--- src/components/experience/Experience.jsx
+++ src/components/experience/Experience.jsx
@@ -14,33 +14,45 @@ const Experience = () => {
           <div className="experience__content">
             <article className='experience__details'>
               <BsPatchCheckFill />
-              <h4>HTML</h4>
-              <small className='text-light'>Experienced</small>
+              <div>
+                <h4>HTML</h4>
+                <small className='text-light'>Experienced</small>
+              </div>
             </article>
             <article className='experience__details'>
               <BsPatchCheckFill />
-              <h4>CSS</h4>
-              <small className='text-light'>Experienced</small>
+              <div>
+                <h4>CSS</h4>
+                <small className='text-light'>Experienced</small>
+              </div>
             </article>
             <article className='experience__details'>
               <BsPatchCheckFill />
-              <h4>JavaScript</h4>
-              <small className='text-light'>Experienced</small>
+              <div>
+                <h4>JavaScript</h4>
+                <small className='text-light'>Experienced</small>
+              </div>
             </article>
             <article className='experience__details'>
               <BsPatchCheckFill />
-              <h4>Bootstarp</h4>
-              <small className='text-light'>Experienced</small>
+              <div>
+                <h4>Bootstarp</h4>
+                <small className='text-light'>Experienced</small>
+              </div>
             </article>
             <article className='experience__details'>
               <BsPatchCheckFill />
-              <h4>React</h4>
-              <small className='text-light'>Experienced</small>
+              <div>
+                <h4>React</h4>
+                <small className='text-light'>Experienced</small>
+              </div>
             </article>
             <article className='experience__details'>
               <BsPatchCheckFill />
-              <h4>Node</h4>
-              <small className='text-light'>Experienced</small>
+              <div>
+                <h4>Node</h4>
+                <small className='text-light'>Experienced</small>
+              </div>
             </article>
           </div>
         </div>
@@ -48,32 +60,42 @@ const Experience = () => {
         {/* END OF FRONTEND */}
 
         <div className="experience__backend">
-        <h3>Backend Development</h3>
+          <h3>Backend Development</h3>
           <div className="experience__content">
             <article className='experience__details'>
               <BsPatchCheckFill />
-              <h4>Node JS</h4>
-              <small className='text-light'>Experienced</small>
+              <div>
+                <h4>Node JS</h4>
+                <small className='text-light'>Experienced</small>
+              </div>
             </article>
             <article className='experience__details'>
               <BsPatchCheckFill />
-              <h4>MongoDB</h4>
-              <small className='text-light'>Experienced</small>
+              <div>
+                <h4>MongoDB</h4>
+                <small className='text-light'>Experienced</small>
+              </div>
             </article>
             <article className='experience__details'>
               <BsPatchCheckFill />
-              <h4>PostgreSQL</h4>
-              <small className='text-light'>Experienced</small>
+              <div>
+                <h4>PostgreSQL</h4>
+                <small className='text-light'>Experienced</small>
+              </div>
             </article>
             <article className='experience__details'>
               <BsPatchCheckFill />
-              <h4>MySQL</h4>
-              <small className='text-light'>Experienced</small>
+              <div>
+                <h4>MySQL</h4>
+                <small className='text-light'>Experienced</small>
+              </div>
             </article>
             <article className='experience__details'>
               <BsPatchCheckFill />
-              <h4>Express JS</h4>
-              <small className='text-light'>Experienced</small>
+              <div>
+                <h4>Express JS</h4>
+                <small className='text-light'>Experienced</small>
+              </div>
             </article>
           </div>
         </div>
