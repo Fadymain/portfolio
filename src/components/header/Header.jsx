239fbb237fef85e49144873048e0cdd034483df7
Adetola <adetolafadairo@gmail.com>
2022-06-06 16:27:34 -0700

    importing header picture

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,9 +1,26 @@
 import React from 'react'
-import 'components/header/header.css'
+import './header.css'
+import CTA from './CTA'
+import ME from '../../assets/my-pic.png'
+import HeaderSocials from './HeaderSocials'
 
 const Header = () => {
   return (
-    <div>Header</div>
+    <header>
+      <div className="container header__container">
+        <h5>Hello I'm</h5>
+        <h1>Adetola Fadairo</h1>
+        <h5 className="text-light">Fullstack Developer</h5>
+        <CTA />
+        <HeaderSocials/>
+
+        <div className="me">
+          <img src={ME} alt="" />
+        </div>
+
+        <a href="contact" className='scroll__down'>Scroll Down</a>
+      </div>
+    </header>
   )
 }
 
