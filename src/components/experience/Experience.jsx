c3827eaf2d66caf14ebdee98397ae1edd19376a2
Adetola <adetolafadairo@gmail.com>
2022-06-06 16:39:42 -0700

    adding SASS to frontend development skills

--- src/components/experience/Experience.jsx
+++ src/components/experience/Experience.jsx
@@ -50,7 +50,7 @@ const Experience = () => {
             <article className='experience__details'>
               <BsPatchCheckFill className='experience__details-icon'/>
               <div>
-                <h4>Node</h4>
+                <h4>SASS</h4>
                 <small className='text-light'>Experienced</small>
               </div>
             </article>
