7c5c226f377dc360979f7a18871ee3a65ee790d5
Adetola <adetolafadairo@gmail.com>
2022-06-06 23:51:05 -0700

    linking portfolio section to nav button

--- src/components/nav/Nav.jsx
+++ src/components/nav/Nav.jsx
@@ -3,8 +3,9 @@ import './nav.css'
 import { AiOutlineHome } from 'react-icons/ai'
 import { AiOutlineUser } from 'react-icons/ai'
 import { BiBook } from 'react-icons/bi'
-import { RiServiceFill } from 'react-icons/ri'
+// import { RiServiceFill } from 'react-icons/ri'
 import { BiMessageSquareDetail } from 'react-icons/bi'
+import {RiBriefcaseLine} from "react-icons/ri"
 import { useState } from 'react'
 
 const Nav = () => {
@@ -14,7 +15,7 @@ const Nav = () => {
       <a href='#' onClick={() => setActiveNav('#')} className={activeNav === '#' ? 'active' : ''}><AiOutlineHome /></a>
       <a href='#about' onClick={() => setActiveNav('#about')} className={activeNav === '#about' ? 'active' : ''}><AiOutlineUser /></a>
       <a href='#experience'  onClick={() => setActiveNav('#experience')} className={activeNav === '#experience' ? 'active' : ''}><BiBook /></a>
-      <a href='#services'  onClick={() => setActiveNav('#services')} className={activeNav === '#services' ? 'active' : ''}><RiServiceFill /></a>
+      <a href='#portfolio'  onClick={() => setActiveNav('#portfolio')} className={activeNav === '#portfolio' ? 'active' : ''}><RiBriefcaseLine /></a>
       <a href='#contact'  onClick={() => setActiveNav('#contact')} className={activeNav === '#contact' ? 'active' : ''}><BiMessageSquareDetail /></a>
     </nav>
   )
